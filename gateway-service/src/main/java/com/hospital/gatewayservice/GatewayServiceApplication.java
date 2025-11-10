8ab78437b7ff5991ccf183660c70552f87c9a045
EL-QASEMY MOHAMED <melqasemy2003@gmail.com>
2025-11-10 23:59:13 +0100

    chore(config): setup base microservices architecture with config server and discovery

---
+++ gateway-service/src/main/java/com/hospital/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,22 @@
+package com.hospital.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    //register dynamically to the eureka discovery service
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+}
---
+++ gateway-service/src/test/java/com/hospital/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
